12bae0c81e453dee13d94a44f5031282d1652e81
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2023-11-17 13:45:51 -0300

    [UPDATE] Componente Avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/clesarjr.png" />
+            <Avatar src={"https://github.com/clesarjr.png"} hasBorder={false} />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
