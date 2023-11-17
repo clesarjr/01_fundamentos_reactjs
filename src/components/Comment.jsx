318ae1f1ba7437a2cc5e856aee61c5a80236c3ff
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2023-11-17 13:24:04 -0300

    [UPDATE] Componente Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,34 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 
 export function Comment() {
     return (
-        <div></div>
+        <div className={styles.comment}>
+            <img src="https://github.com/clesarjr.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Clésar Sapelli Junior</strong>
+                            <time title="17 de Novembro às 11:13h" dateTime="2023-17-11 11:13:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
     )
 }
